5a3c22342af2bd4bee70d8e9677281f624070426
Jakub Nurski <nurskijakub@gmail.com>
2016-08-06 21:06:24 +0200

    Changed 'test_home_page_returns_correct_html' to exclude CSRF token.

--- lists/tests.py
+++ lists/tests.py
@@ -3,6 +3,8 @@ from django.test import TestCase
 from django.http import HttpRequest
 from django.template.loader import render_to_string
 
+import re
+
 from lists.views import home_page
 
 
@@ -15,4 +17,22 @@ class HomePageTest(TestCase):
         request = HttpRequest()
         response = home_page(request)
         expected_html = render_to_string('home.html')
-        self.assertEqual(response.content.decode(), expected_html)
+        response_html = response.content.decode()
+
+        match = re.search(
+            "((.|\n)*)<input type='hidden[^\/]*\/>((.|\n)*)",
+            response_html
+        )
+        matched_groups = match.groups()
+        response_html = str(matched_groups[0]) + str(matched_groups[2])
+
+        self.assertEqual(response_html, expected_html)
+
+    def test_home_page_can_save_a_POST_request(self):
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST['item_text'] = 'A new list item'
+
+        response = home_page(request)
+
+        self.assertIn('A new list item', response.content.decode())
