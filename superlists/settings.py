18a299a53bd6ab1502d633e117ce9589184da1c0
Jakub Nurski <nurskijakub@gmail.com>
2016-08-15 01:03:27 +0200

    Change setting.py to sqlite3 backend.

--- superlists/settings.py
+++ superlists/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'superlists.wsgi.application'
 if 'RDS_DB_NAME' in os.environ:
     DATABASES = {
         'default': {
-            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'ENGINE': 'django.db.backends.sqlite3',
             'NAME': os.environ['RDS_DB_NAME'],
             'USER': os.environ['RDS_USERNAME'],
             'PASSWORD': os.environ['RDS_PASSWORD'],
