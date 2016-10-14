b46650c4e8eedbb040f4a6bd2be015b90e6c77f6
Jakub Nurski <nurskijakub@gmail.com>
2016-10-15 00:06:14 +0200

    Server tools spike added.

---
+++ functional_tests/server_tools.py
@@ -0,0 +1,10 @@
+from os import path
+import subprocess
+THIS_FOLDER = path.dirname(path.abspath(__file__))
+
+
+def create_session_on_server(host, email):
+    pass
+
+def reset_database(host):
+    pass
