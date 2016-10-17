1aaa6a661cf00ccbacf0e0c74f613e4002636903
Jakub Nurski <nurskijakub@gmail.com>
2016-10-17 22:49:59 +0200

    Changed REST API url. Redefined create_pre_authenticated_session view.

--- rest_api/views.py
+++ rest_api/views.py
@@ -1,5 +1,8 @@
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
+from rest_framework import status, permissions
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
 from rest_framework.renderers import JSONRenderer
 from accounts.models import Token
 from accounts.serializers import TokenSerializer
@@ -9,25 +12,23 @@ from django.contrib.auth import (
 )
 from django.contrib.sessions.backends.db import SessionStore
 
+
 User = get_user_model()
 
 
-class JSONResponse(HttpResponse):
-
-    def __init__(self, data, **kwargs):
-        content = JSONRenderer().render(data)
-        kwargs['content_type'] = 'application/json'
-        super(JSONResponse, self).__init__(content, **kwargs)
-
-
-@csrf_exempt
+@api_view(['GET'])
+@permission_classes((permissions.AllowAny, ))
 def create_pre_authenticated_session(request, email):
-    try:
-        user = User.objects.get(email=email)
-    except User.DoesNotExist:
-        user = User.objects.create(email=email)
-    session = SessionStore()
-    session[SESSION_KEY] = user.pk
-    session[BACKEND_SESSION_KEY] = settings.AUTHENTICATION_BACKENDS[0]
-    session.save()
-    return JSONResponse({"session_key": session.session_key})
+    if request.method == 'GET':
+        try:
+            user = User.objects.get(email=email)
+        except User.DoesNotExist:
+            user = User.objects.create(email=email)
+        session = SessionStore()
+        session[SESSION_KEY] = user.pk
+        session[BACKEND_SESSION_KEY] = settings.AUTHENTICATION_BACKENDS[0]
+        session.save()
+        return Response({"session_key": session.session_key})
+
+    else:
+        return Response(status=status.HTTP_400_BAD_REQUEST)
--- superlists/urls.py
+++ superlists/urls.py
@@ -9,6 +9,6 @@ urlpatterns = [
     url(r'^$', list_views.home_page, name='home'),
     url(r'^lists/', include(list_urls)),
     url(r'^accounts/', include(accounts_urls)),
-    url(r'^rest-api/', include(rest_api_urls)),
+    url(r'^rest/', include(rest_api_urls)),
     # url(r'^admin/', include(admin.site.urls)),
 ]
